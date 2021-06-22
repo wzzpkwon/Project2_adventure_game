4985c559efe885c5e07f3266983571677aa33767
wzzpkwon <77912170+wzzpkwon@users.noreply.github.com>
2021-06-22 01:37:12 -0400

    Update adventure_game.py

--- Project2_Adventure_Game/adventure_game.py
+++ Project2_Adventure_Game/adventure_game.py
@@ -2,12 +2,14 @@ import time
 import random
 import sys
 
-# function to print messages with pause
+
+# function to print messages with pause.
 def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
 
-# function to validate input and to repeat input messages until valid input is entered.
+
+# function to validate input.
 def valid_input(prompt, options):
     while True:
         response = input(prompt).lower()
@@ -15,7 +17,8 @@ def valid_input(prompt, options):
             if option in response:
                 return response
 
-# function to print introductions that sets the tone in the beginning of a game.
+
+# function to print introductions for the game.
 def intro():
     print_pause("In the middle of a dead-silent night, "
                 "you are standing in the lobby of a building.")
@@ -28,10 +31,12 @@ def intro():
     print_pause("To your right is the security system control panel.")
     print_pause("To your left is a computer.\n")
 
+
 # function to return to the lobby prior to take next course of action.
 def to_lobby(tasks, options, passcodes):
     print_pause("You return to the lobby.")
     bank_robbery(tasks, options, passcodes)
+
 
 # function to work on control panel for security devices.
 def control_panel(tasks, options, passcodes):
@@ -46,7 +51,8 @@ def control_panel(tasks, options, passcodes):
         tasks.append("deactivate")
     to_lobby(tasks, options, passcodes)
 
-# function to work on computer to hack the computer and to download security code.
+
+# function to hack the computer and download security codes.
 def hack_computer(tasks, options, passcodes):
     print_pause("You approach the computer.")
     if "security_code" in tasks:
@@ -67,6 +73,7 @@ def hack_computer(tasks, options, passcodes):
                         "by the security camera!")
             play_again(options)
 
+
 # function to work on vault door.
 def open_vault(tasks, options, passcodes):
     print_pause("You approach the vault.")
@@ -74,47 +81,44 @@ def open_vault(tasks, options, passcodes):
                 "to access the vault.")
     print_pause("Would you like to 1) enter the security code or "
                 "2) crack the vault?")
-    while True:
-        response = valid_input("(Please enter 1 or 2)\n", options)
-        if response == "1":
-            if "deactivate" in tasks:
-                if "security_code" in tasks:
-                    enter_passcode = input("Please enter the security code:\n")
-                    if enter_passcode == passcodes:
-                        print_pause("The vault door finally opens up.")
-                        print_pause("You've made yourself into the vault "
-                                    "full of cash! Congratulations!")
-                    else:
-                        print_pause("The alarm sets off "
-                                    "as you have entered a wrong password.")
-                        print_pause("You are busted!")
-                    play_again(options)
+    response = valid_input("(Please enter 1 or 2)\n", ['1', '2'])
+    if response == "1":
+        if "deactivate" in tasks:
+            if "security_code" in tasks:
+                enter_passcode = input("Please enter the security code:\n")
+                if enter_passcode == passcodes:
+                    print_pause("The vault door finally opens up.")
+                    print_pause("You've made yourself into the vault "
+                                "full of cash! Congratulations!")
                 else:
-                    print_pause("You don’t have the security code "
-                                "to access the vault.")
-                    to_lobby(tasks, options, passcodes)
-            else:
-                print_pause("The alarm sets off as you attempt "
-                            "to enter the security code.")
-                print_pause("You forgot to deactivate "
-                            "the security devices...")
-                print_pause("You are busted!")
+                    print_pause("The alarm sets off "
+                                "as you have entered a wrong password.")
+                    print_pause("You are busted!")
                 play_again(options)
-        elif response == "2":
-            if "deactivate" in tasks:
-                print_pause("You miraculously cracked the vault door open.")
-                print_pause("With sheer delight and sense of accomplishment, "
-                            "you step into the vault.")
-                print_pause("Suddenly, a hidden trap door appears and "
-                            "locks you up inside the vault.")
-                print_pause("Oh no, you are trapped!")
             else:
-                print_pause("The alarm sets off as you attempt to "
-                            "force your entry.")
-                print_pause("You are busted!")
-            play_again(options)
+                print_pause("You don’t have the security code "
+                            "to access the vault.")
+                to_lobby(tasks, options, passcodes)
         else:
-            return response
+            print_pause("The alarm sets off as you attempt "
+                        "to enter the security code.")
+            print_pause("You forgot to deactivate "
+                        "the security devices...")
+            print_pause("You are busted!")
+            play_again(options)
+    elif response == "2":
+        if "deactivate" in tasks:
+            print_pause("You miraculously cracked the vault door open.")
+            print_pause("With sheer delight and sense of accomplishment, "
+                        "you step into the vault.")
+            print_pause("Suddenly, a hidden trap door appears and "
+                        "locks you up inside the vault.")
+            print_pause("Oh no, you are trapped!")
+        else:
+            print_pause("The alarm sets off as you attempt to "
+                        "force your entry.")
+            print_pause("You are busted!")
+        play_again(options)
 
 
 # function to select appropriate protocol for a bank heist!
@@ -131,21 +135,23 @@ def bank_robbery(tasks, options, passcodes):
     elif response == "3":
         open_vault(tasks, options, passcodes)
 
+
 # function to ask the player to give options to replay or exit the game.
 def play_again(options):
     response = valid_input("Would you like to play again? (y/n)", options)
-    if response == "N":
+    if response == "n":
         print_pause("Enough for the day, huh? Thanks for playing!")
-    elif response == "Y":
+    elif response == "y":
         print_pause("Great! Please wait while the game is restarting...")
         main()
     else:
         print_pause("Please enter a valid input: ")
 
+
 # main function that executes the game.
 def main():
     tasks = []
-    options = ['1', '2', '3', 'Y', 'N']
+    options = ['1', '2', '3', 'y', 'n']
     passcodes = str(random.randrange(1000, 10000))
     intro()
     bank_robbery(tasks, options, passcodes)
